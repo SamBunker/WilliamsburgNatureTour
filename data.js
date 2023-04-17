4b2104a56db67d4efc04472cc52905946e6683ba
Not Sam <samuelbunker@live.com>
2023-04-17 14:07:47 -0400

    Added Pollinator Garden Image Embed

--- data.js
+++ data.js
@@ -281,7 +281,7 @@ var APP_DATA = {
           "yaw": 0.3348289761119929,
           "pitch": 0.332311852489239,
           "title": "Flower Stems",
-          "text": "The stalks of the plants are left standing over the winter because other insects lay their eggs in the stems of the wildflowers and overwinter there. Also, the seeds of the wildflowers feed overwintering birds during cold winters. We finally cut the stems each spring to allow sun to reach the regrowth of the perennial wildflowers but also it keeps trees from turning this meadow into a forest in the natural process of succession."
+          "text": "The stalks of the plants are left standing over the winter because other insects lay their eggs in the stems of the wildflowers and overwinter there. Also, the seeds of the wildflowers feed overwintering birds during cold winters. We finally cut the stems each spring to allow sun to reach the regrowth of the perennial wildflowers but also it keeps trees from turning this meadow into a forest in the natural process of succession. <img src='./img/PollinatorGarden.png' alt='Beautiful pollinator garden.' style='width:220px;height:200px;'>"
         },
         {
           "yaw": -2.8574748943978143,
